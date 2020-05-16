63993a8e8a3b65a7270fa518415774af01230347
Luke Nowland <LukeNowlandFilms@gmail.com>
2020-05-16 03:08:00 -0400

    Portal implementation and fading finished

--- Assets/Scripts/Scene Management/Portal.cs
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,13 +8,57 @@ namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier { A, B, C }
+
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] float fadeOutTime = 1f;
+        [SerializeField] float fadeWaitTime = 1f;
+        [SerializeField] float fadeInTime = .5f;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Player")
             {
-                SceneManager.LoadSceneAsync(sceneToLoad);
+                StartCoroutine(Transition());
             }
+        }
+
+        IEnumerator Transition()
+        {
+            if (sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to Load not set");
+                yield break;
+            }
+
+            DontDestroyOnLoad(gameObject);
+            Fader fader = FindObjectOfType<Fader>();
+            yield return fader.FadeOut(fadeOutTime);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+            yield return fader.FadeIn(fadeInTime);
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            var player = GameObject.FindWithTag("Player");
+            player.transform.position = otherPortal.spawnPoint.transform.position;
+            player.transform.rotation = otherPortal.spawnPoint.transform.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (var portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.destination != this.destination) continue;
+                return portal;
+            }
+            return null;
         }
     }
 }

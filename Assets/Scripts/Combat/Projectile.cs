1a886149745503a790ffe27c7190f8a9c545c947
Luke Nowland <LukeNowlandFilms@gmail.com>
2020-06-03 00:09:34 -0400

    Added arrow, projectile follows target

---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] Transform target = null;
+    [SerializeField] float speed = 5f;
+
+    void Update()
+    {
+        if (target == null) return;
+        transform.LookAt(GetAimLocation());
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private Vector3 GetAimLocation()
+    {
+        var targetCapsule = target.GetComponent<CapsuleCollider>();
+        if (targetCapsule == null) return target.position;
+        return target.position + Vector3.up * (targetCapsule.height/2);
+    }
+}
